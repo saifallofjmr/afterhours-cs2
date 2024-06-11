d55b53b4d1e57ed0afd79df9f4076c3a820903f8
delabahmani <delabahmani@gmail.com>
2024-06-10 20:16:04 -0400

    implement news

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const Card = ({ name, image, content }: { name: string; image: string; content: string }) => {
+  return (
+    <div className="bg-black poppins flex h-[500px] w-[500px] rounded-lg py-6 text-center text-2xl font-semibold text-white shadow-lg flex-col">
+      <Image src={image} width={10000} height={10000} alt="counter-strike bot profile picture" className="h-60 w-full rounded-lg"/>
+      <h5 className="">{name}</h5>
+      <p className="">{content}</p>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
@@ -22,6 +31,17 @@ export default function Home() {
             DISCORD
           </Link>
         </button>
+      </div>
+    </div>
+
+    <div className="flex items-center justify-center mt-20 flex-col">
+      <h3 className="text-bluegray">LATEST</h3>
+      <h1 className="text-bluegray text-5xl text-center poppins font-bold border-b-2">NEWS</h1>
+      <div className="m-10 -z-10 circle rounded-lg border-white border-[1px]">
+      <Card name="After Hours Season 2" image="/assets/cash-cup.png" content="Summer Break Cash Cup [1000 USD]" />
+      </div>
+      <div>
+
       </div>
     </div>
 
--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Card = ({ name, image }: { name: string; image: string }) => {
   return (
-    <div className="bg-[#1434A4] poppins flex h-60 w-64 items-center justify-center rounded-lg py-6 text-center text-2xl font-semibold text-white shadow-lg flex-col">
-      <Image src={image} width={1000} height={1000} alt="counter-strike bot profile picture" className="h-40 w-40"/>
+    <div className="bg-[#28282B] poppins flex h-60 w-64 items-center justify-center rounded-lg py-6 text-center text-2xl font-semibold text-white shadow-lg flex-col">
+      <Image src={image} width={1000} height={1000} alt="counter-strike bot profile picture" className="h-40 w-40 rounded-full"/>
       <h5 className="">{name}</h5>
 
     </div>
@@ -13,13 +13,13 @@ const Card = ({ name, image }: { name: string; image: string }) => {
 export default function Staff() {
   return (
     <div>
-      <h1 className="text-white text-5xl text-center font-bold ">STAFF</h1>
-      <p className="text-white text-center text-xl montserrat ">
+      <h1 className="text-white text-5xl text-center font-bold m-5">STAFF</h1>
+      <p className="text-white text-center text-xl montserrat">
         Our staff is here to help. We are dedicated to the cause. All of our
         members are actively engaged in the community and working hard for our
         growth!
       </p>
-      <div className="grid grid-cols-4 gap-10 ">
+      <div className="grid grid-cols-4 gap-10  mt-10">
         <Card image="/assets/ct-pfp.png" name="mousee" />
         <Card image="/assets/ct-pfp.png" name="pink ocean yuki" />
         <Card image="/assets/ct-pfp.png" name="costerfunk" />
