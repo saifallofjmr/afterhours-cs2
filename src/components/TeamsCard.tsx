72b1254196498abdc709c2f4477ae2bc830ac990
saifallofjmr <saifallofjmr@gmail.com>
2024-07-20 05:26:50 -0400

    Update TeamsCard.tsx
    extending feature set

--- src/components/TeamsCard.tsx
+++ src/components/TeamsCard.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+type Player = {
+  name: string;
+  faceitUrl: string;
+  twitchUrl?: string; // Optional since not all players may have a Twitch link
+};
+
 type TeamCardProps = {
   imageUrl: string;
   title: string;
-  players: string[];
+  players: Player[];
 };
 
 export default function TeamsCard({ imageUrl, title, players }: TeamCardProps) {
@@ -12,19 +18,29 @@ export default function TeamsCard({ imageUrl, title, players }: TeamCardProps) {
       <img
         src={imageUrl}
         alt={title}
-        className="h-60 w-36 rounded-r-lg object-cover md:h-60 md:w-60"
+        className="h-60 w-36 rounded-r-lg object-cover md:h-full md:w-60"
       />
       <div className="flex flex-col justify-between leading-normal">
         <h1 className="mb-4 text-sm font-bold tracking-tight text-start px-2 text-white md:mb-2 md:px-5 md:text-2xl">
           {title}
         </h1>
         <ul>
-          {players.map((players, index) => (
+          {players.map((player, index) => (
             <li
               className="cursor-pointer px-3 py-[3px] text-start text-sm text-white hover:text-accent md:px-5 md:text-lg"
               key={index}
             >
-              {players}
+              <a href={player.faceitUrl} target="_blank" rel="noopener noreferrer" className="hover:underline">
+                {player.name}
+              </a>
+              {player.twitchUrl && (
+                <>
+                  {" - "}
+                  <a href={player.twitchUrl} target="_blank" rel="noopener noreferrer" className="hover:underline">
+                    Twitch
+                  </a>
+                </>
+              )}
             </li>
           ))}
         </ul>
@@ -32,3 +48,4 @@ export default function TeamsCard({ imageUrl, title, players }: TeamCardProps) {
     </div>
   );
 }
+
